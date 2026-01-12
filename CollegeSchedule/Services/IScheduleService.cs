758c8af3264a62624fc5cdbcc3acafa9861f3fea
opuR1 <lel070320@gmail.com>
2026-01-12 20:58:30 +0700

    Реализованы ScheduleSerices

---
+++ CollegeSchedule/Services/IScheduleService.cs
@@ -0,0 +1,9 @@
+﻿using CollegeSchedule.DTO;
+namespace CollegeSchedule.Services
+{
+    public interface IScheduleService
+    {
+        Task<List<ScheduleByDateDto>> GetScheduleForGroup(string groupName, DateTime
+       startDate, DateTime endDate);
+    }
+}
