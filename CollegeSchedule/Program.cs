dc323d3319153a6499c5e427113141253714fb05
opuR1 <lel070320@gmail.com>
2026-01-12 21:12:14 +0700

    Исправлен Program.cs

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -3,33 +3,39 @@ using CollegeSchedule.Middlewares;
 using CollegeSchedule.Services;
 using Microsoft.EntityFrameworkCore;
 
-    var builder = WebApplication.CreateBuilder(args);
-    // Add services to the container.
-    DotNetEnv.Env.Load();
-    var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
-         $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
-         $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
-         $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
-        $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
+var builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(connectionString));
-    builder.Services.AddScoped<IScheduleService, ScheduleService>();
-    builder.Services.AddControllers();
+// Add services to the container.
+DotNetEnv.Env.Load();
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+    $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+    $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+    $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+    $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 
-    // Learn more about configuring Swagger/OpenAPI at
-    https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    var app = builder.Build();
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-    app.UseHttpsRedirection();
-    app.UseMiddleware<ExceptionMiddleware>();
-    app.UseAuthorization();
-    app.UseRouting();
-    app.MapControllers();
-    app.Run();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IScheduleService, ScheduleService>();
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseRouting();
+app.MapControllers();
+
+app.Run();
