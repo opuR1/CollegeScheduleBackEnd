0dac6559743ac0ea741b118dfb25f7b778ccb241
opuR1 <lel070320@gmail.com>
2026-01-12 20:42:16 +0700

    Создан проект и сделана папка Models с классами

---
+++ CollegeSchedule/Models/Specialty.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CollegeSchedule.Models
+{
+    [Table("specialties")]
+    public class Specialty
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ CollegeSchedule/Models/Subject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace CollegeSchedule.Models
+{
+    [Table("subject")]
+    public class Subject
+    {
+        [Key]
+        [Column("subject_id")]
+        public int SubjectId { get; set; }
+        [Column("name")]
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
