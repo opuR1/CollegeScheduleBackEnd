94d26744053933ff81473b1a991dd11d2d7e6509
opuR1 <lel070320@gmail.com>
2026-01-12 21:05:48 +0700

    Реализован класс Program

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -1,25 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
+using CollegeSchedule.Data;
+using CollegeSchedule.Middlewares;
+using CollegeSchedule.Services;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+    var builder = WebApplication.CreateBuilder(args);
+    // Add services to the container.
+    DotNetEnv.Env.Load();
+    var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+         $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+         $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
+         $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
+        $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(connectionString));
+    builder.Services.AddScoped<IScheduleService, ScheduleService>();
+    builder.Services.AddControllers();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    // Learn more about configuring Swagger/OpenAPI at
+    https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var app = builder.Build();
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseHttpsRedirection();
+    app.UseMiddleware<ExceptionMiddleware>();
+    app.UseAuthorization();
+    app.UseRouting();
+    app.MapControllers();
+    app.Run();
