7816cb2397c65b423cda205d442205cc2a6e878d
opuR1 <lel070320@gmail.com>
2026-01-13 12:28:25 +0700

    Исправлены ошибки с Nuget пакетами

--- CollegeSchedule/Program.cs
+++ CollegeSchedule/Program.cs
@@ -7,13 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 DotNetEnv.Env.Load();
-var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};" + 
     $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
     $"Database={Environment.GetEnvironmentVariable("DB_NAME")};" +
     $"Username={Environment.GetEnvironmentVariable("DB_USER")};" +
     $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}";
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+builder.Services.AddDbContext<AppDbContext>(options =>options.UseNpgsql(connectionString));
 
 builder.Services.AddScoped<IScheduleService, ScheduleService>();
 builder.Services.AddControllers();
